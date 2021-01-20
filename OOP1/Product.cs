eb36fb20378ea2551bc7e4fa40dafbd970847b28
ZEYNEP ERDEN <zzeyneperdenn@gmail.com>
2021-01-21 00:07:55 +0300

    20.01.2021 End of Class

---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snipped = hazir kodlar
+    class Product
+    {
+        public int Id { get; set; } //primary key
+        public int CategoryId { get; set; } //foreign key, boyle yazariz genelde
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //CRUD = Creat Read Update Delete
+    }
+}
