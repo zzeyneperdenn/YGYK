aeffa940c2962d2a8f0fb1d175a6e6c571a5564d
ZEYNEP ERDEN <zzeyneperdenn@gmail.com>
2021-01-27 15:33:56 +0300

    Day 4 Homework is done.

---
+++ D4Homework/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D4Homework
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] _key;
+        Tvalue[] _value;
+
+        public MyDictionary()
+        {
+            _key = new Tkey[0];
+            _value = new Tvalue[0];
+        }
+
+        public void Add(Tkey item1, Tvalue item2)
+        {
+            Tkey[] _tempkey = _key;
+            Tvalue[] _tempValue = _value;
+
+            _key = new Tkey[_key.Length + 1];
+            _value = new Tvalue[_value.Length + 1];
+
+            for (int i = 0; i < _tempkey.Length; i++)
+            {
+                _key[i] = _tempkey[i];
+            }
+            _key[_key.Length - 1] = item1;
+
+            for (int j = 0; j < _tempValue.Length; j++)
+            {
+                _value[j] = _tempValue[j];
+            }
+            _value[_value.Length - 1] = item2;
+        }
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+
+        public Tkey[] Item1
+        {
+            get { return _key; }
+
+        }
+        public Tvalue[] Item2
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ D4Homework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace D4Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(0, "Zeynep");
+            myDictionary.Add(1, "Zeliha");
+            myDictionary.Add(2, "İlayda");
+            myDictionary.Add(3, "Hilal");
+            myDictionary.Add(4, "Kubra");
+            Console.WriteLine(myDictionary.Count);
+
+            foreach (var my in myDictionary.Item1)
+            {
+                Console.WriteLine(my);
+            }
+
+            foreach (var my in myDictionary.Item2)
+            {
+                Console.WriteLine(my);
+            }
+        }
+    }
+}
